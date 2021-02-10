72f43336503bc5dc2e63eaa2d566e84786883de0
astatsuya <sharekobekko@gmail.com>
2021-02-10 23:13:10 +0900

    first commit

---
+++ src/models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+taskSchema.pre("save", async function (next) {
+  next();
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
