6dffbc3880eca0afffdcd44599b2bd8265e8e96b
astatsuya <sharekobekko@gmail.com>
2021-02-14 14:59:21 +0900

    feature: add profile test

--- tests/user.test.js
+++ tests/user.test.js
@@ -99,4 +99,38 @@ describe("user", () => {
   it("Should not delete account for unauthenticateuser", async () => {
     await request(app).delete("/users/me").send().expect(401);
   });
+
+  it("Should upload avatar image", async () => {
+    await request(app)
+      .post("/users/me/avatar")
+      .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+      .attach("avatar", "tests/fixtures/profile-pic.jpg")
+      .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+  });
+
+  it("Should update valid user fields", async () => {
+    await request(app)
+      .patch("/users/me")
+      .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+      .send({
+        name: "Jess",
+      })
+      .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe("Jess");
+  });
+
+  it("Should not update invalid user fields", async () => {
+    await request(app)
+      .patch("/users/me")
+      .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+      .send({
+        location: "Philadelphia",
+      })
+      .expect(400);
+  });
 });
