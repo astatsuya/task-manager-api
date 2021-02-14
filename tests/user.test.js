860b43dd63f2a49aadf9a0b2eedf74ca28efeffd
astatsuya <sharekobekko@gmail.com>
2021-02-14 12:55:58 +0900

    feature: add auth test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
 describe("user", () => {
+  const userOneId = new mongoose.Types.ObjectId();
+
   const userOne = {
+    _id: userOneId,
     name: "Mike",
     email: "mike@example.com",
     password: "56what!!",
+    tokens: [
+      {
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+      },
+    ],
   };
 
   beforeEach(async () => {
@@ -40,7 +49,7 @@ describe("user", () => {
       .expect(200);
   });
 
-  it("Should login nonwxistent user", async () => {
+  it("Should login nonexistent user", async () => {
     await request(app)
       .post("/users/login")
       .send({
@@ -48,5 +57,17 @@ describe("user", () => {
         password: "MyPas1234!!",
       })
       .expect(500);
+  });
+
+  it("Should get profile for user", async () => {
+    await request(app)
+      .get("/users/me")
+      .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+      .send()
+      .expect(200);
+  });
+
+  it("Should not get profile for unauthenticated user", async () => {
+    await request(app).get("/users/me").send().expect(401);
   });
 });
